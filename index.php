8c051edd91fde250ba9a49cd6c701f91c6c0dee5
MisterGrimalkin <loki@amarantha.net>
2016-06-05 11:42:21 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+    <?php
+    include("includes/header.html");
+    include("includes/common.php");
+    include("includes/wrapper.php");
+    ?>
+
+    <body onload="onLoad()">
+
+        <div style="padding:5px; font-size: large;">
+            <label for="ipaddress">LightBoard IP</label>&nbsp;<input id="ipaddress" name="ipaddress" type="text" onchange="loadBoard();"/>
+        </div>
+
+        <iframe id="board" name="board" width="1400" height="1000" src="" frameborder="0" style="float: left;"></iframe>
+
+    </body>
+
+</html>
+
+
